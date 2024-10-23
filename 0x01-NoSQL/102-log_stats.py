c1c521e1528ee1373cfa57e511a6b4bbd35d6220
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-10-23 05:50:40 +0300

    Create 102-log_stats.py

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+playground
+"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    coll = client.logs.nginx
+    total_logs = coll.count_documents({})
+    method_get = coll.count_documents({"method": "GET"})
+    method_post = coll.count_documents({"method": "POST"})
+    method_put = coll.count_documents({"method": "PUT"})
+    method_patch = coll.count_documents({"method": "PATCH"})
+    method_delete = coll.count_documents({"method": "DELETE"})
+    status_check = coll.count_documents({"method": "GET", "path": "/status"})
+
+    result = (
+        "{} logs\nMethods:\n"
+        "\tmethod GET: {}\n"
+        "\tmethod POST: {}\n"
+        "\tmethod PUT: {}\n"
+        "\tmethod PATCH: {}\n"
+        "\tmethod DELETE: {}\n"
+        "{} status check"
+    ).format(total_logs, method_get, method_post,
+             method_put, method_patch, method_delete, status_check)
+    print(result)
+
+    p_counts = coll.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    print("IPs:")
+    for ip in ip_counts:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
