35123a5bf74b8bffcdcc880f230131f6439c195f
AmrTaman <amrtaman4325@gmail.com>
2024-10-24 02:26:58 +0000

    get done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ we are caching using redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,3 +25,27 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: bytes,
+            fn: Optional[Callable]) -> Optional[Union[str, bytes, int, float]]:
+        """
+        we are getting from redis
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        converting bytes into str directly
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        converting bytes to int
+        """
+        return self.get(key, fn=int)
